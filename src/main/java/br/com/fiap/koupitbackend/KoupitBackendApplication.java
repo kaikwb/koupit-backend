1f374729c8facb91288ba96984b36076ee422652
Kaik Bassanelli <kaikwb@hotmail.com>
2023-12-03 06:03:04 +0900

    Initial commit

---
+++ src/main/java/br/com/fiap/koupitbackend/KoupitBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.koupitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoupitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoupitBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/koupitbackend/KoupitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.koupitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoupitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
